67b4c36ed8c324d14edbd193670594963ae55b39
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-08-30 14:27:40 +0500

    Add services + sln

---
+++ Services/Kaihatsu.NotificationManager.NotificationAPI/Controllers/TemplatesApiController.cs
@@ -0,0 +1,49 @@
+﻿using Kaihatsu.NotificationManager.Core.Interfaces;
+using Kaihatsu.NotificationManager.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Kaihatsu.NotificationManager.NotificationAPI.Controllers;
+
+[Route("api/templates")]
+public class TemplatesApiController : ControllerBase
+{
+    private readonly IRepositoryAsync<Template> _repository;
+    public TemplatesApiController(IRepositoryAsync<Template> repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var templates = await _repository.GetAllAsync();
+
+        List<TemplateDTO> templatesList = new List<TemplateDTO>();
+        foreach (Template template in templates)
+        {
+            templatesList.Add(new TemplateDTO
+            {
+                Id = template.Id,
+                Name = template.Name,
+                Title = template.Title,
+                Content = Encoding.UTF8.GetString(template.Content)//Convert.ToBase64String(templates.First().Content)//
+            });
+        }
+
+        return Ok(templatesList);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(TemplateDTO item)
+    {
+        Template template = new Template
+        {
+            Name = item.Name,
+            Title = item.Title,
+            Content = Encoding.UTF8.GetBytes(item.Content)
+        };
+        await _repository.AddAsync(template);
+        return Ok(item);
+    }
+}
---
+++ Services/Kaihatsu.NotificationManager.NotificationAPI/MessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Kaihatsu.NotificationManager.NotificationAPI;
+
+public class MessageDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int TemplateId { get; set; }
+    public string SenderType { get; set; }
+    public string Recipient { get; set; }
+    public string Content { get; set; }
+}
